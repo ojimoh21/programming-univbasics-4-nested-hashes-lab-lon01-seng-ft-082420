f36c057ed17b0491514862a5c9f5c082cd892372
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-03-26 21:05:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,11 @@ def adding_matz
 # }
 # return the entire updated hash
 
-programmer_hash[:]
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", 
+:languages => ["LISP", "C"]
+  
+  programmer_hash
+}
 
 	programmer_hash = 
  		{

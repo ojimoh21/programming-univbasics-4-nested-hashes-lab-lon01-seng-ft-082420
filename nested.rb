d401fb8703b384cc01c663fb76e67fe2f3aa6d98
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-03-26 21:37:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,12 +15,12 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
       }
     }
 
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -80,7 +83,11 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
+}
+
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  
+  programmer_hash
 
 end
 
@@ -103,6 +110,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +135,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    programmer_hash
 
 end
